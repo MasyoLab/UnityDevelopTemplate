e1656124ff9934a0c0746833a4dd8d9aed9d04fd
masyo-lab <masyo.lab@gmail.com>
2021-08-07 03:38:53 +0900

    update

--- Assets/SQLite/Scripts/SqliteDatabase.cs
+++ Assets/SQLite/Scripts/SqliteDatabase.cs
@@ -31,46 +31,46 @@ public class SqliteDatabase {
     const int SQLITE_BLOB = 4;
     const int SQLITE_NULL = 5;
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_open")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_open")]
     private static extern int sqlite3_open(string filename, out IntPtr db);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_close")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_close")]
     private static extern int sqlite3_close(IntPtr db);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_prepare_v2")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_prepare_v2")]
     private static extern int sqlite3_prepare_v2(IntPtr db, string zSql, int nByte, out IntPtr ppStmpt, IntPtr pzTail);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_step")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_step")]
     private static extern int sqlite3_step(IntPtr stmHandle);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_finalize")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_finalize")]
     private static extern int sqlite3_finalize(IntPtr stmHandle);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_errmsg")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_errmsg")]
     private static extern IntPtr sqlite3_errmsg(IntPtr db);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_column_count")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_column_count")]
     private static extern int sqlite3_column_count(IntPtr stmHandle);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_column_name")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_column_name")]
     private static extern IntPtr sqlite3_column_name(IntPtr stmHandle, int iCol);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_column_type")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_column_type")]
     private static extern int sqlite3_column_type(IntPtr stmHandle, int iCol);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_column_int")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_column_int")]
     private static extern int sqlite3_column_int(IntPtr stmHandle, int iCol);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_column_text")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_column_text")]
     private static extern IntPtr sqlite3_column_text(IntPtr stmHandle, int iCol);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_column_double")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_column_double")]
     private static extern double sqlite3_column_double(IntPtr stmHandle, int iCol);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_column_blob")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_column_blob")]
     private static extern IntPtr sqlite3_column_blob(IntPtr stmHandle, int iCol);
 
-    [DllImport("sqlite3", EntryPoint = "sqlite3_column_bytes")]
+    [DllImport("libsqliteX", EntryPoint = "sqlite3_column_bytes")]
     private static extern int sqlite3_column_bytes(IntPtr stmHandle, int iCol);
 
     [DllImport("libsqliteX", EntryPoint = "sqlite3_exec")]
